879a89950f966c4a80d32f59fceb98e7868407e4
Edson Gonzalo Mamani Veizaga <edson.nur@gmail.com>
2022-05-25 19:23:47 -0400

    add

--- DmsCheckIn/Infrastruture/src/java/ExtensionsInfrastructure.java
+++ DmsCheckIn/Infrastruture/src/java/ExtensionsInfrastructure.java
@@ -2,8 +2,6 @@ import EF.Contexts.IWriteDbContext;
 import EF.Contexts.MongoDb.WriteDbContext;
 import EF.Repository.CheckInRepository;
 import EF.UnitOfWork;
-import MomoryRepository.MemoryCheckInRepository;
-import MomoryRepository.MemoryDataBase;
 import Repositories.IUnitOfWork;
 import Repositories.IcheckInRepository;
 import extensions.IServiceCollection;
@@ -11,17 +9,11 @@ import extensions.IServiceCollection;
 public class ExtensionsInfrastructure {
 
     public static void AddInfraestructure(){
-//        Extensions.AddApplication();
-//        IServiceCollection.AddSingleton(MemoryDataBase.class);
-//        IServiceCollection.AddScoped(IUnitOfWork.class, UnitOfWork.class);
-//        IServiceCollection.AddScoped(IcheckInRepository.class, MemoryCheckInRepository.class);
 
         Extensions.AddApplication();
         IServiceCollection.AddMediator();
         IServiceCollection.AddScoped(IWriteDbContext.class, WriteDbContext.class);
         IServiceCollection.AddScoped(IUnitOfWork.class, UnitOfWork.class);
         IServiceCollection.AddScoped(IcheckInRepository.class, CheckInRepository.class);
-        //IServiceCollection.AddScoped(IAsientoRepository.class, AsientoRepository.class);
-        //IServiceCollection.AddScoped(IMarcaRepository.class, MarcaRepository.class);
     }
 }
