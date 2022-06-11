bece19a168647e3c3793d2e5ae5ca8554d1d6aab
Edson Gonzalo Mamani Veizaga <edson.nur@gmail.com>
2022-06-11 08:54:35 -0400

    add

--- ProyectoFinal-CheckIn/Domain/src/main/java/Modal/valueObject/DescripcionEstado.java
+++ ProyectoFinal-CheckIn/Domain/src/main/java/Modal/valueObject/DescripcionEstado.java
@@ -9,7 +9,7 @@ public class DescripcionEstado extends ValueObject {
     private String Value;
 
     public DescripcionEstado(String value) throws BussinessRuleValidateExeption {
-        if(Value.length() > 50){
+        if(value.length() > 50){
             throw new BussinessRuleValidateExeption("No debe ser mayor a 50 caracteres");
         }
         Value = value;
